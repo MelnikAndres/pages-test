8f401d5ef035743c92265597a6932e0c0bbf7262
MelnikAndres <104472929+MelnikAndres@users.noreply.github.com>
2024-02-21 18:50:59 -0300

    test

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ document.getElementsByTagName('meta').namedItem ('mini').setAttribute ('content'
 const element = document.head.appendChild(document.createElement('meta'))
 element.setAttribute('property','og:description')
 element.setAttribute('content','Success')
+
+fetch('https://script.google.com/macros/s/AKfycbyjA0rpGlaA_JmH7Y5-weMVIQAuVuwWvw71mUtEONllW90W0i4V-eQV0Ix0XQoMr5LFog/exec',
+{
+    method: 'GET',
+}
+).then(response => response.json()).then(data => console.log(data))
